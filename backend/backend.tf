d436a3f1532fca84c0f53906b62b8f27b1149ad9
yubin425 <yubin6gkrsus@gmail.com>
2025-03-20 07:47:39 +0000

    fix: 퍼블릭 액세스 문제 해결

--- backend/backend.tf
+++ backend/backend.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.terraform_state.id
   block_public_acls       = false
-  block_public_policy     = false
+  #block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
